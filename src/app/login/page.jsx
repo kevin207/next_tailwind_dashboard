6e26fe303c73e763a59a164ffdadb1aaa78df282
Kevin Tanuwijaya <kevin.119140207@student.itera.ac.id>
2023-06-14 20:20:52 +0700

    turbopack + Login Icon

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 
+import { AiFillLock } from "react-icons/ai";
+import { MdEmail } from "react-icons/md";
+
 const Login = () => {
   return (
     <div className="h-screen z-0  w-screen bg-slate-300 flex justify-center items-center">
       {/* BG + OVERLAY */}
+      <div className="bg-slate-800 z-10 absolute w-[100%] h-[100%] opacity-60" />
       <Image
         src="/background.jpg"
         alt="login background"
@@ -13,10 +17,9 @@ const Login = () => {
         height={1080}
         className="absolute object-cover w-screen h-screen"
       />
-      <div className="bg-slate-800 z-10 absolute w-[100%] h-[100%] opacity-50" />
 
       {/* LOGIN FORM */}
-      <div className="bg-white z-20 bg-opacity-80 text-slate-700 px-8 py-14 rounded-lg shadow-md w-[500px] ">
+      <div className="bg-white z-20 bg-opacity-80 text-slate-700 px-10 pt-12 pb-14 rounded-xl shadow-md w-[500px] ">
         {/* LOGO */}
         <div className="flex items-center justify-center">
           <div className="mb-6 w-[200px] relative h-[200px]">
@@ -31,27 +34,46 @@ const Login = () => {
         </div>
 
         {/* FORM CONTENT */}
-        <div className="text-3xl font-bold mb-2 ">Login Form</div>
-        <form className="flex flex-col gap-3 font-medium">
+        <div className="text-3xl font-bold mb-4">Login Form</div>
+        <form className="flex flex-col gap-3 font-semibold">
           <div>
             <div>Username</div>
-            <input
-              type="text"
-              className="w-[100%] border-2 rounded-md px-2 py-1"
-            />
+            <div className="flex border-2 rounded-md items-stretch bg-white">
+              <div className="px-2 border-r-2">
+                <MdEmail className="h-[100%]" />
+              </div>
+
+              <input
+                type="text"
+                className="w-[100%] px-2 py-1 outline-slate-500"
+              />
+            </div>
           </div>
 
           <div>
             <div>Password</div>
-            <input
-              type="password"
-              className="w-[100%] border-2 rounded-md px-2 py-1"
-            />
+            <div className="flex border-2 rounded-md items-stretch bg-white">
+              <div className="px-2 border-r-2">
+                <AiFillLock className="h-[100%]" />
+              </div>
+
+              <input
+                type="password"
+                className="w-[100%] px-2 py-1 outline-slate-500"
+              />
+            </div>
           </div>
 
           <Link
+            href="/"
+            className="hover:underline self-end text-sm cursor-pointer"
+          >
+            Forgot Password?
+          </Link>
+
+          <Link
             href={"/dashboard"}
-            className="bg-slate-700 rounded-md mt-2 text-white py-2 flex justify-center items-center border-2 border-slate-700 hover:bg-transparent hover:text-slate-700 transition-all ease-out duration-200"
+            className="bg-slate-700 rounded-md mt-2 text-white py-2 flex justify-center items-center border-2  hover:bg-slate-800  transition-all ease-out duration-200"
           >
             Login
           </Link>
