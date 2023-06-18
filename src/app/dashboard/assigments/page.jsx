7f2b8e979a0e814c483e298891b33c1624fa2336
Kevin Tanuwijaya <kevin.119140207@student.itera.ac.id>
2023-06-18 19:22:43 +0700

    Refine Drawer

--- src/app/dashboard/assigments/page.jsx
+++ src/app/dashboard/assigments/page.jsx
@@ -10,6 +10,37 @@ const Assigments = () => {
     },
   ];
 
+  const profiles = [
+    {
+      id: 1,
+      photo:
+        "https://c.ndtvimg.com/2020-08/h5mk7js_cat-generic_625x300_28_August_20.jpg",
+      name: "Oliver",
+      alias: "The Cat",
+    },
+    {
+      id: 2,
+      photo:
+        "https://www.hartz.com/wp-content/uploads/2022/04/small-dog-owners-1.jpg",
+      name: "Bruno",
+      alias: "The Dog",
+    },
+    {
+      id: 3,
+      photo:
+        "https://img.freepik.com/premium-vector/red-dragon-head-illustration_113398-564.jpg?w=2000",
+      name: "Shen",
+      alias: "The Dragon",
+    },
+    {
+      id: 4,
+      photo:
+        "https://play-lh.googleusercontent.com/8QnH9AhsRfhPott7REiFUXXJLRIxi8KMAP0mFAZpYgd44OTOCtScwXeb5oPe1E4eP4oF",
+      name: "Mbek",
+      alias: "The Goat",
+    },
+  ];
+
   return (
     <>
       {/* BREADCUMBS 92-4 = 88 */}
@@ -17,45 +48,32 @@ const Assigments = () => {
       <div className="h-[88vh] items-center justify-center flex flex-col gap-4">
         <div>Assigment</div>
 
-        <div className="w-64 carousel rounded-box">
-          <div className="carousel-item w-full" id="item1">
-            <img
-              src="https://www.theme-junkie.com/wp-content/uploads/wordpress-carousel-plugin.png"
-              className="w-full"
-              alt="Tailwind CSS Carousel component"
-            />
-          </div>
+        <div className="flex flex-col">
+          {profiles.map((profile) => (
+            <div
+              key={profile.id}
+              className="flex flex-row justify-between bg-white px-4 py-2 items-center w-[400px] border-b-2"
+            >
+              <div className="flex gap-4 items-center">
+                <div className="avatar">
+                  <div className="w-14 rounded-full">
+                    <img alt="profile avatar" src={profile.photo} />
+                  </div>
+                </div>
+                <div className="flex flex-col">
+                  <strong>{profile.name}</strong>
+                  <div className="text-gray-400 font-semibold text-sm">
+                    {profile.alias}
+                  </div>
+                  <small className="text-gray-400">Popular</small>
+                </div>
+              </div>
 
-          <div className="carousel-item w-full" id="item2">
-            <img
-              src="https://upload.jaknot.com/2022/12/images/products/2ff82a/thumbnail/kotak-musik-merry-go-round-musical-box-carousel-hd-yyh.jpg"
-              className="w-full"
-              alt="Tailwind CSS Carousel component"
-            />
-          </div>
-
-          <div className="carousel-item w-full" id="item3">
-            <img
-              src="https://img.freepik.com/free-vector/carousel-horse-concept-illustration_114360-9386.jpg?w=2000"
-              className="w-full"
-              alt="Tailwind CSS Carousel component"
-            />
-          </div>
-        </div>
-
-        <div className="flex justify-center w-full py-2 gap-2">
-          <a
-            href="#item1"
-            className="btn btn-xs focus:bg-slate-700 focus:text-white"
-          >
-            1
-          </a>
-          <a href="#item2" className="btn btn-xs">
-            2
-          </a>
-          <a href="#item3" className="btn btn-xs">
-            3
-          </a>
+              <button className="px-2 py-1 bg-sky-500 text-white text-base font-bold rounded-[5px] hover:bg-sky-600 transition-all ease-in-out duration-300">
+                Follow
+              </button>
+            </div>
+          ))}
         </div>
       </div>
     </>
--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -2,7 +2,6 @@
 import { useState } from "react";
 import {
   MdKeyboardArrowLeft,
-  MdKeyboardArrowRight,
   MdDashboard,
   MdSupervisorAccount,
   MdAssignmentTurnedIn,
@@ -12,42 +11,31 @@ import Link from "next/link";
 
 const Drawer = () => {
   const [drawer, setDrawer] = useState(true);
+  const [active, setActive] = useState("dashboard");
 
   return (
     <div
-      className={
-        drawer == true
-          ? "w-[15vw] h-[100vh] bg-secondary flex justify-between flex-col transition-all ease-in duration-150"
-          : "w-[4vw] h-[100vh] bg-secondary flex justify-between flex-col transition-all ease-in duration-150"
-      }
+      className={`w-[15vw] h-[100vh] bg-secondary flex justify-between flex-col transition-all ease-in-out duration-400 ${
+        drawer == false && "w-[4vw]"
+      }`}
     >
       {/* TOP */}
       <div className="text-tertiary sticky top-0 font-normal bg-primary flex h-[8vh] justify-center items-center shadow-sm">
-        {drawer == true ? (
-          <>
-            <FaAtlassian className="text-3xl" />
-            <div className=" text-xl">tlassian</div>
-          </>
-        ) : (
-          <FaAtlassian className="text-3xl" />
-        )}
+        <FaAtlassian className="text-3xl" />
+        <div className={`text-xl transition-all ease-in ${drawer == false && "opacity-0 absolute duration-0"}`}>
+          tlassian
+        </div>
       </div>
 
       {/* MIDDLE */}
       <div className="text-white h-[86vh]">
         {/* FIRST MENU */}
-        <div
-          className={
-            drawer == true ? "mt-16" : "mt-16 flex flex-col items-center"
-          }
-        >
+        <div className={`mt-16 flex flex-col`}>
           {/* MENU TITLE */}
           <div
-            className={
-              drawer == true
-                ? "px-4 text-md font-medium mb-1 text-tertiary"
-                : "hidden"
-            }
+            className={`px-4 text-md font-medium mb-1 text-tertiary ${
+              drawer == false && "opacity-0"
+            }`}
           >
             MENU
           </div>
@@ -55,62 +43,56 @@ const Drawer = () => {
           {/* MENU ITEMS */}
           <Link
             href={"/dashboard"}
-            className={
-              drawer == true
-                ? "text-tertiary flex items-center gap-4 py-5 px-4 cursor-pointer transition-all ease-in duration-200 hover:bg-slate-700"
-                : "text-tertiary flex items-center justify-center py-5 w-[100%] cursor-pointer transition-all ease-in duration-200 hover:bg-slate-700"
-            }
+            onClick={() => setActive("dashboard")}
+            className={`flex w-[100%] flex-row gap-4 px-4 py-5 text-slate-200 items-center transition-all ease-in-out duration-300 hover:bg-slate-700 ${
+              active == "dashboard" && "bg-slate-700"
+            } ${
+              drawer == false &&
+              "justify-center text-slate-100 group-hover:justify-normal"
+            }`}
           >
-            {drawer == true ? (
-              <>
-                <MdDashboard className="text-2xl" />
-                <div className="text-md font-medium">Dashboard</div>
-              </>
-            ) : (
-              <>
-                <MdDashboard className="text-2xl" />
-              </>
-            )}
+            <MdDashboard className="text-2xl" />
+            <div
+              className={`text-md font-medium transition-all ease-in ${
+                drawer == false && "duration-0 opacity-0 absolute"
+              }`}
+            >
+              Dashboard
+            </div>
           </Link>
 
           <Link
-            href="/dashboard/students"
-            className={
-              drawer == true
-                ? "text-tertiary flex items-center gap-4 py-5 px-4 cursor-pointer transition-all ease-in duration-200 hover:bg-slate-700"
-                : "text-tertiary flex items-center justify-center py-5 w-[100%] cursor-pointer transition-all ease-in duration-200 hover:bg-slate-700"
-            }
+            href={"/dashboard/students"}
+            onClick={() => setActive("students")}
+            className={`flex w-[100%] flex-row gap-4 px-4 py-5 text-slate-200 items-center transition-all ease-in-out duration-300 hover:bg-slate-700 ${
+              active == "students" && "bg-slate-700"
+            } ${drawer == false && "justify-center text-slate-100"}`}
           >
-            {drawer == true ? (
-              <>
-                <MdSupervisorAccount className="text-2xl" />
-                <div className="text-md font-medium">Students</div>
-              </>
-            ) : (
-              <>
-                <MdSupervisorAccount className="text-2xl" />
-              </>
-            )}
+            <MdSupervisorAccount className="text-2xl" />
+            <div
+              className={`text-md font-medium  transition-all ease-in ${
+                drawer == false && "duration-0 opacity-0 absolute"
+              }`}
+            >
+              Students
+            </div>
           </Link>
 
           <Link
-            href="/dashboard/assigments"
-            className={
-              drawer == true
-                ? "text-tertiary flex items-center gap-4 py-5 px-4 cursor-pointer transition-all ease-in duration-200 hover:bg-slate-700"
-                : "text-tertiary flex items-center justify-center py-5 w-[100%] cursor-pointer transition-all ease-in duration-200 hover:bg-slate-700"
-            }
+            href={"/dashboard/assigments"}
+            onClick={() => setActive("assigments")}
+            className={`flex w-[100%] flex-row gap-4 px-4 py-5 text-slate-200 items-center transition-all ease-in-out duration-300 hover:bg-slate-700 ${
+              active == "assigments" && "bg-slate-700"
+            } ${drawer == false && "justify-center text-slate-100"}`}
           >
-            {drawer == true ? (
-              <>
-                <MdAssignmentTurnedIn className="text-2xl" />
-                <div className="text-md font-medium">Assignments</div>
-              </>
-            ) : (
-              <>
-                <MdAssignmentTurnedIn className="text-2xl" />
-              </>
-            )}
+            <MdAssignmentTurnedIn className="text-2xl" />
+            <div
+              className={`text-md font-medium  transition-all ease-in ${
+                drawer == false && "duration-0 opacity-0 absolute"
+              }`}
+            >
+              Assigments
+            </div>
           </Link>
         </div>
       </div>
@@ -120,17 +102,15 @@ const Drawer = () => {
         onClick={() => {
           setDrawer(!drawer);
         }}
-        className={
-          drawer == true
-            ? "text-tertiary cursor-pointer font-semibold text-md bg-primary flex h-[6vh] justify-end items-center px-6 transition-all ease-in duration-250 hover:text-slate-400"
-            : "text-tertiary cursor-pointer font-semibold text-md bg-primary flex h-[6vh] justify-end items-center px-3 transition-all ease-in duration-250 hover:text-slate-400"
-        }
+        className={`text-tertiary cursor-pointer font-semibold text-md bg-primary flex h-[6vh] justify-end items-center px-6 transition-all ease-in duration-250 hover:text-slate-400 ${
+          drawer == false && "px-3"
+        }`}
       >
-        {drawer == true ? (
-          <MdKeyboardArrowLeft className="text-3xl " />
-        ) : (
-          <MdKeyboardArrowRight className="text-3xl " />
-        )}
+        <MdKeyboardArrowLeft
+          className={`text-3xl transition-all ease-in duration-200 ${
+            drawer == false && "rotate-180"
+          }`}
+        />
       </div>
     </div>
   );
